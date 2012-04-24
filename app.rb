75bcea40fd7ab1bccb4b8ea381ab845991ebcddb
Alan deLevie <adelevie@gmail.com>
2012-04-24 17:30:59 -0400

    initial commit

--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
+require 'json'
+
 class App < Sinatra::Base
   get "/" do
-    "Hello World"
+    if params[:agency_id] or params[:service_id] or params[:tag]
+      c = Client.new
+      @url = c.get_accounts(params).uri.to_str
+    end
+    erb :index
+  end
+  
+  get "/test" do
+    erb :test
+  end
+end
+
+class Client < Weary::Client
+  domain "http://registry.usa.gov"
+    
+  get :get_accounts, "/accounts.json" do |resource|
+    resource.optional :agency_id, :service_id, :tag
   end
 end
